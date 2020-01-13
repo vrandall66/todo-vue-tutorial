430d676dec109421b3c81114667e62c4f0f16e00
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-13 14:45:02 -0700

    Render fetched todos, add and delete todos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    async deleteTodo(id) {
+      const options = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        options
+      );
+
+      if (!response.ok) {
+        throw new Error(`Could not delete todo, please try again later.`);
+      }
+
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    async addTodo(newTodo) {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTodo)
+      };
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos',
+        options
+      );
+      if (!response.ok) {
+        throw new Error(`Could not add new todo, please try again later.`);
+      }
+      const data = await response.json();
+
+      this.todos = [...this.todos, data];
+    }
+  },
+  async mounted() {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos?_limit=15'
+    );
+    if (!response.ok) {
+      throw new Error(`Could not get todos, please try again later.`);
+    }
+    const data = await response.json();
+    this.todos = data;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
